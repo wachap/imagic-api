fdd9e9eb007e7c00736e457d32d8d78db34479c3
wochap <w4ch4pgmail.com>
2015-06-30 17:34:49 -0500

    update ImageController.php

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -1,7 +1,7 @@
 <?php namespace App\Http\Controllers;
 
 // Utils
-// use App\Utils\Files\ImageFileManager;
+use App\Utils\Files\ImageFileManager;
 
 // Laravel
 use Illuminate\Http\Request;
@@ -9,15 +9,51 @@ use App\Http\Controllers\Controller;
 
 class ImageController extends Controller {
 
+	protected $imageManager;
+
+	public function __construct (ImageFileManager $imageFileManager)
+	{
+		$this->imageManager = $imageFileManager;
+	}	
+
 	/**
 	 *	Retorna una imagen y cambia el tamaño si es necesario
 	 *	GET /images/{$photoName}
 	 *	@param string $photoName
 	 * 	@return Response image cache
 	 */
-	public function read (Request $request, $photoName)
-	{
+	public function show (Request $request, $photoName)
+	{	
+		// Obtener query params widt y height		
+		$width  = $request->get('width');
+		$height = $request->get('height');
+		if (!$request->get('width'))
+		{
+			$width = 9999;
+		}
+		if (!$request->get('height'))
+		{
+			$height = 9999;
+		}
 
+		// Si no existe la imagen retorno 404
+		$this->imageManager->setFileName($photoName);
+		if (!$this->imageManager->exists())
+		{
+			if (Request::ajax()) {
+				return response()->json([], 404);
+			}
+			throw new NotFoundHttpException;
+		}
+
+		if ($request->get('download') == true)
+		{
+			return response()->download($this->imageManager->getFilePath());
+		}
+		
+		// Retorno la imagen pedida y cambio de tamaño segun el query param
+		$imageCache = $this->imageManager->resize($width, $height);
+		return response()->make($imageCache, 200, [ 'Content-type' => 'image/jpeg' ]);
 	}
 
 	/**
@@ -25,9 +61,9 @@ class ImageController extends Controller {
 	 *	@param string $photoName
 	 * 	@return Response image download
 	 */
-    public function download ($photoName)
+    public function download ()
     {
-
+    	// return response()->download($this->imageManager->getFilePath());
     }
 
 }
