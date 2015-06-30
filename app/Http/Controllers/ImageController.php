c119c8cb2ab2d965503731c6e922f18e102b8a68
Gean Carlos <w4ch4p@gmail.com>
2015-06-30 09:08:26 -0500

    create image controller

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,33 @@
+<?php namespace App\Http\Controllers;
+
+// Utils
+use App\Utils\Files\ImageFileManager;
+
+// Laravel
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ImageController extends Controller {
+
+	/**
+	 *	Retorna una imagen y cambia el tamaño si es necesario
+	 *	GET /images/{$photoName}
+	 *	@param string $photoName
+	 * 	@return Response image cache
+	 */
+	public function read (Request $request, $photoName)
+	{
+
+	}
+
+	/**
+	 *	Retorna la descarga de la imagen original
+	 *	@param string $photoName
+	 * 	@return Response image download
+	 */
+    public function download ($photoName)
+    {
+
+    }
+
+}
