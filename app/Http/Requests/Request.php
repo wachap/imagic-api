3c9283f84ec6f4fc3d6fbbe12b3f6966c16d4a9f
wochap <w4ch4pgmail.com>
2015-06-30 17:06:15 -0500

    add custom response

--- app/Http/Requests/Request.php
+++ app/Http/Requests/Request.php
@@ -6,5 +6,21 @@ use Illuminate\Foundation\Http\FormRequest;
 
 abstract class Request extends FormRequest
 {
-    //
+
+	/**
+	 *	Custom Response
+	 */
+	public function response(array $errors)
+	{
+		return response()->json(array(
+			"message" => "Validacion fallida",
+			"errors"  => $errors ),
+			422
+		);
+		// if ($this->ajax())
+		// {}
+		// return redirect()->back()
+		// 	->withInput($this->except($this->dontFlash))
+		// 	->withErrors($errors);
+	}
 }
