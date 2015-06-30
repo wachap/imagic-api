81dac0214461af44e0492ce138fbba31f4aca7d8
wochap <w4ch4pgmail.com>
2015-06-30 17:06:58 -0500

    update PhotoController.php

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -10,9 +10,6 @@ use App\Http\Requests\Photo\CreateRequest as CreatePhotoRequest;
 use App\Http\Requests\Photo\UpdateRequest as UpdatePhotoRequest;
 use App\Http\Requests\Photo\ModifyRequest as ModifyPhotoRequest;
 
-// Utils
-use App\Utils\Files\ImageFileManager;
-
 // Laravel
 use App\Http\Controllers\Controller;
 use Illuminate\Routing\Route;
@@ -21,44 +18,45 @@ class PhotoController extends Controller {
 
 	public function __construct (PhotoRepo $photoRepo, PhotoManager $photoManager)
 	{
-        $this->repository   = $photoRepo;
+		$this->repository   = $photoRepo;
 		$this->manager 		= $photoManager;
 
-		$this->beforeFilter('@find', ['only' => ['show', 'update', 'modify', 'delete']] );
+		$this->beforeFilter('@find', ['only' => ['show', 'update']] );
+		$this->beforeFilter('@findWithTrashed', ['only' => ['delete', 'modify']] );
 	}
 
 	/**
 	 * 	Muestra una lista de todos los resources.
-     *  @return response json $resources
+	 *  @return response json $resources
 	 */
 	public function read ()
 	{
-		$resources = $this->photoRepo->all();
+		$resources = $this->repository->all();
 		return response()->json($resources, 200);
 	}
 
-    /**
-     *  Cambia el estado(trashed) de varios resources.
-     *  @return response json message
-     */
-    public function modifyMany ()
+	/**
+	 *  Cambia el estado(trashed) de varios resources.
+	 *  @return response json message
+	 */
+	public function modifyMany ()
 	{
-
+		return response()->json(["message" => "modifyMany"], 200);
 	}
 
-    /**
-     *  Elimina definitivamente varios resources
-     *  @return response json message
-     */
-    public function deleteMany ()
-    {
-
-    }
+	/**
+	 *  Elimina definitivamente varios resources
+	 *  @return response json message
+	 */
+	public function deleteMany ()
+	{
+		return response()->json(["message" => "deleteMany"], 200);
+	}
 
 	/**
 	 *	Retorna el resource en formato JSON.
-     *  @param int $id
-     *  @return response json $resource
+	 *  @param int $id
+	 *  @return response json $resource
 	 */
 	public function show ()
 	{
@@ -67,47 +65,54 @@ class PhotoController extends Controller {
 
 	/**
 	 *	Crea un nuevo resource en la BD.
-     *  @return response json $resource
+	 *  @return response json $resource
 	 */
 	public function create (CreatePhotoRequest $request)
 	{
-		$data = $request->only($this->manager->createFilterData());
-		
+		$data     = $request->only($this->manager->getCreateFields());
+		$response = $this->manager->create($data);
+		return response()->json($response, 201);
 	}
 
 	/**
 	 * 	Actualiza el resource.
-     *  @return response json $resource
+	 *  @return response json $resource
 	 */
 	public function update (UpdatePhotoRequest $request)
 	{
-
+		$data     = $request->only($this->manager->getUpdateFields());
+		$response = $this->manager->update($data, $this->resource);
+		return response()->json($response, 200);
 	}
 
 	/**
 	 * 	Cambia el estado(trashed) del resource.
-     *  @return response json message
+	 *  @return response json message
 	 */
 	public function modify (ModifyPhotoRequest $request)
 	{
-
+		$data     = $request->only($this->manager->getModifyFields());
+		$response = $this->manager->modify($data, $this->resource);
+		return response()->json($response, 200);
 	}
 
 	/**
 	 *	Elimina el resource de la bd.
-     *  @return response json message
+	 *  @return response json message
 	 */
 	public function delete ()
 	{
-
+		$response = $this->manager->delete($this->resource);
+		return response()->json($response, 200);
 	}
 
 	/**
 	 *	Muestra todos los resources eliminados.
-         *  @return response json $resourcesTrashed
+	 *  @return response json $resourcesTrashed
 	 */
 	public function readTrashes ()
 	{
-
+		$resources = $this->repository->onlyTrashed();
+		return response()->json($resources, 200);
 	}
 }
