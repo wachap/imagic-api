a2e5abf3d1c32b151a37af90582f98cb2677879d
wochap <w4ch4pgmail.com>
2015-06-30 17:06:42 -0500

    update PhotoManager.php

--- app/Models/Managers/PhotoManager.php
+++ app/Models/Managers/PhotoManager.php
@@ -1,40 +1,88 @@
-<?php
+<?php namespace App\Models\Managers;
 
-namespace App\Models\Managers;
-
-use App\Models\Entities\Photo;
+// Utils
+use App\Utils\Files\ImageFileManager;
 
 class PhotoManager {
 
-	public function createFilterData ()
+	protected $imageManager;
+
+	public function __construct (ImageFileManager $imageFileManager)
 	{
-		return [
-            'name',
-            'image',
-        ];
+		$this->imageManager = $imageFileManager;
 	}
 
-    public function updateFilterData ()
+	public function getCreateFields ()
 	{
 		return [
-            'name',
-        ];
+			'name',
+			'image',
+		];
 	}
 
-    public function modifyFilterData ()
+	public function getUpdateFields ()
 	{
 		return [
-            'trashed',
-        ];
+			'name',
+		];
+	}
+
+	public function getModifyFields ()
+	{
+		return [
+			'trashed',
+		];
 	}
 
 	public function prepareData ($data)
-	{
+	{		
+		$url      = $this->imageManager->getPhotoUrl();
+		$fileName = $this->imageManager->getFileName();
+		
+		$data     = array_add($data, 'url', $url);		
+		$data     = array_add($data, 'fileName', $fileName);	
 		return $data;
 	}
 
-    public function getResponse ()
-    {
+	public function getMessageAndModify ($trashed, $resource) 
+	{
+		if ($trashed) 
+		{
+			$resource->delete();
+			return "Se ha movido el elemento a la papelera";
+		}
+		$resource->restore();
+		return "Se ha restaurado el elemento en la biblioteca";
+	}
 
-    }
+	public function create ($data)
+	{
+		$this->imageManager->save( $data['image'] );
+
+		$data     = $this->prepareData($data);
+		$resource = Photo::create($data);
+		$response = ["message" => "1 foto nueva", "photo" => $resource];
+		return $response;
+	}
+
+	public function update ($data, $resource)
+	{
+		$resource->update($data);
+		$response = ["message" => "Se edito la foto correctamente", "photo" => $resource];
+		return $response;
+	}
+
+	public function modify ($data, $resource)
+	{
+		$resource->update($data);
+		$response = ["message" => $this->getMessageAndModify($data['trashed'], $resource), "photo" => $resource];		
+		return $response;
+	}
+
+	public function delete ($resource)
+	{
+		$resource->forceDelete();
+		$response = ["message" => "Se ha eliminado definitivamente el elemento"];
+		return $response;
+	}
 }
