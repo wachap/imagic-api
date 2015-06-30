2636dafcb914442d32797d41afe7d85fc03672eb
Gean Carlos <w4ch4p@gmail.com>
2015-06-30 08:38:51 -0500

    update routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,23 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/images/{fileName}', ['uses' => 'ImageController@show']);
+
+/*
+**	Photo Resource
+*/
+Route::group(['prefix' => 'api/v1'], function () {
+    Route::get('photos/trashes', ['uses' => 'PhotoController@readTrashes']);
+    Route::get('photos', ['uses'         => 'PhotoController@read']);
+    Route::post('photos', ['uses'        => 'PhotoController@create']);
+    Route::patch('photos', ['uses'       => 'PhotoController@modifyMany']);
+    Route::delete('photos', ['uses'      => 'PhotoController@deleteMany']);
+
+    Route::get('photos/{id}', ['uses'    => 'PhotoController@show']);
+    Route::put('photos/{id}', ['uses'    => 'PhotoController@update']);
+    Route::patch('photos/{id}', ['uses'  => 'PhotoController@modify']);
+    Route::delete('photos/{id}', ['uses' => 'PhotoController@delete']);
 });
