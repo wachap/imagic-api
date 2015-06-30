393e7e4cef7d8f7f3c8f6d16aaaf37c9d94d2c48
Gean Carlos <w4ch4p@gmail.com>
2015-06-30 09:30:22 -0500

    update controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,21 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 abstract class Controller extends BaseController
 {
     use DispatchesJobs, ValidatesRequests;
+
+    protected $resource;
+    protected $repository;
+
+    /**
+     *	Search the specified resource.
+     */
+    public function find (Route $route)
+    {
+        $id             = $route->getParameter('id');
+        $this->resource = $this->repository->find($id);
+
+        if (!$this->resource)
+        {
+            return response()->json([], 404);
+        }
+    }
 }
